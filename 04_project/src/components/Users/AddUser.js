c2055ba8e4e0f9f477b70d768c4591f0bfba5171
D.O <d.o@kakaoenterrpise.com>
2021-12-11 00:04:52 +0900

    first commit

---
+++ 04_project/src/components/Users/AddUser.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
+  };
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label className="username">Username</label>
+        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
+        <label className="age">Age (Years)</label>
+        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
