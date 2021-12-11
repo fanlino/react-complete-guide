a2ec4cdacdb25fdfe36d78b3410dd067a7229eb3
fanlino <fanlino@gmail.com>
2021-12-11 10:58:47 +0900

    fragment, portal

--- 08_project/src/components/Users/AddUser.js
+++ 08_project/src/components/Users/AddUser.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
-import ErrorModal from "../UI/ErroModal";
+import ErrorModal from "../UI/ErrorModal";
+import Wrapper from "../Helpers/Wrapper";
 
 import classes from "./AddUser.module.css";
 
@@ -48,7 +49,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <Wrapper>
       {error && (
         <ErrorModal
           title={error.title}
@@ -75,7 +76,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
